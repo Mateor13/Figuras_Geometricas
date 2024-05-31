6a9121037af213180cbcffaac1810f1232f55b5a
1-21-01-E043-13\POO <vwilmer111@gmail.com>
2024-05-31 12:43:57 -0500

    Wilmer Vargas

--- src/Figuras/dosD.java
+++ src/Figuras/dosD.java
@@ -1,4 +1,55 @@
 package Figuras;
 
 public class dosD extends Figurasgeometricas{
+    double area;
+    double perimetro;
+    double lado;
+    public dosD(){}
+    public dosD(int numero_lados, String nombre, double area, double perimetro, double lado){
+        super(numero_lados, nombre);
+        this.area = area;
+        this.perimetro = perimetro;
+        this.lado = lado;
+
+    }
+    // getters and setters
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public double getPerimetro() {
+        return perimetro;
+    }
+
+    public void setPerimetro(double perimetro) {
+        this.perimetro = perimetro;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+    // metodos personalizados
+
+    public Double Perimetro(double perimetro, int numero_lados){
+        perimetro = lado * numero_lados;
+        return perimetro;
+    }
+    public  Double area(){
+
+    }
+
+
+
+
+
+
+
 }
