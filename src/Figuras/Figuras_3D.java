23729454b62016866befb05273bb3d1e2880d36e
POO <richard.soria@epn.edu.ec>
2024-05-31 12:33:22 -0500

    Atributos y métodos clase Figurasgeometricas

---
+++ src/Figuras/Figuras_3D.java
@@ -0,0 +1,12 @@
+package Figuras;
+
+public class Figuras_3D extends Figurasgeometricas
+{
+    int numero_caras;
+    double arista;
+    int numero_vertices;
+
+    public Figuras_3D() {}
+
+
+}
--- src/Figuras/Figurasgeometricas.java
+++ src/Figuras/Figurasgeometricas.java
@@ -1,4 +1,36 @@
 package Figuras;
 
-public class Figurasgeometricas {
+public class Figurasgeometricas
+{
+    int numero_lados;
+    String nombre;
+
+    public Figurasgeometricas() {}
+
+    public Figurasgeometricas(int numero_lados, String nombre) {
+        this.numero_lados = numero_lados;
+        this.nombre = nombre;
+    }
+
+    public int getNumero_lados() {
+        return numero_lados;
+    }
+
+    public void setNumero_lados(int numero_lados) {
+        this.numero_lados = numero_lados;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void mostrar_datos()
+    {
+        System.out.println("Numero Lados: " + numero_lados);
+        System.out.println("Nombre: " + nombre);
+    }
 }
