818df9f60ed696b6b37f41a3605ed2e364b0b9d9
Mateor13 <mateo.torres@epn.edu.ec>
2024-06-05 17:53:22 -0500

    Irregulares

--- src/Figuras/Irregulares.java
+++ src/Figuras/Irregulares.java
@@ -1,10 +1,43 @@
 package Figuras;
 
-public class Irregulares extends dosD{
- Double vertices, angulosInternos, Diagonales;
+public class Irregulares extends dosD {
+    private double[] x;
+    private double[] y;
+    private int n;
 
-    public Irregulares() {
+    public Irregulares(double[] x, double[] y) {
+        if (x.length != y.length) {
+            throw new IllegalArgumentException("Las coordenadas x e y deben tener la misma longitud");
+        }
+        this.x = x;
+        this.y = y;
+        this.n = x.length;
     }
 
+    @Override
+    public Double calcularPerimetro() {
+        double perimetro = 0.0;
+        for (int i = 0; i < n; i++) {
+            int siguiente = (i + 1) % n;
+            perimetro += Math.sqrt(Math.pow(x[siguiente] - x[i], 2) + Math.pow(y[siguiente] - y[i], 2));
+        }
+        return perimetro;
+    }
+
+    @Override
+    public Double calcularArea() {
+        double area = 0.0;
+        for (int i = 0; i < n; i++) {
+            int siguiente = (i + 1) % n;
+            area += x[i] * y[siguiente] - y[i] * x[siguiente];
+        }
+        area = Math.abs(area) / 2.0;
+        return area;
+    }
+    @Override
+    public void mostrar_datos () {
+        System.out.println("Perímetro: " + calcularPerimetro());
+        System.out.println("Área: " + calcularArea());
+    }
 }
 
